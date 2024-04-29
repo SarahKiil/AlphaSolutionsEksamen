60538a7d0abea345ec0b75c5ab3c42439c66c280
SarahKiil <sami0001@stud.kea.dk>
2024-04-29 11:38:43 +0200

    new

---
+++ src/main/java/com/example/alphasolutionseksamen/AlphaSolutionsEksamenApplication.java
@@ -0,0 +1,13 @@
+package com.example.alphasolutionseksamen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphaSolutionsEksamenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlphaSolutionsEksamenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/alphasolutionseksamen/AlphaSolutionsEksamenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.alphasolutionseksamen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlphaSolutionsEksamenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
